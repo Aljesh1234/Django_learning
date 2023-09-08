d8b75cd2f4bf8a5aab227a3d20e4284b94b9b261
Aljesh1234 <rautaljesh@gmail.com>
2023-09-08 16:47:14 +0545

    update

---
+++ home/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import *
+
+
+
+
+urlpatterns =[
+    path("",home),
+    path("about/",about),
+    path("contact/",contact),
+    path("portfolio/",portfolio),
+    path("price/",price),
+    path("services/",services)
+]
--- home/views.py
+++ home/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+def home(request):
+    views = {}
+    views['services'] = Services.objects.all()
+    views['Feedbacks'] = Feedback.objects.all()
+    return render(request,'index.html',views)
+
+def about(request):
+    return render(request, 'about.html')
+def contact(request):
+    return render(request,'contact.html')
+def portfolio(request):
+    return render(request,'portfolio.html')
+def price(request):
+    return render(request,'price.html')
+def services(request):
+    views = {}
+    views['new_service'] = Services.objects.all()
+
+    return render(request,'services.html',views)
